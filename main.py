ee7354ece6dce940188f77c73c6587b3096ed042
TheoFABIEN <fabien.theo@gmail.com>
2024-10-06 12:04:16 +0200

    Added scoring

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ G_WIDTH = 10
 G_HEIGHT = 20
 
 # Initial speed
-INIT_SPEED = .5
+INIT_SPEED = .1
 
 SHAPES_LIST = [
     l_shape, j_shape,
@@ -25,8 +25,11 @@ class Grid:
 
     def __init__(self, width = 10, height = 20, speed = .5):
 
-        self.game_over = False
-        self.fall_speed_mult = 1
+        self.game_over = False   
+        self.score = 0
+        self.points_granted = [40, 100, 300, 1200]  # Points for 1/2/3/4 lines
+        self.fall_speed_mult = 1    
+        self.frame_counter = 0
         self.width = width
         self.height = height
         self.speed = speed
@@ -36,6 +39,8 @@ class Grid:
         ]
         self.current_shape = None
         self.current_shape_location = (0, 0) # row, column
+
+
 
 
     def update_cells(self, row, col, value = 1):
@@ -57,7 +62,10 @@ class Grid:
             if value
         ]
         for coord in shape_coords:
-            self.update_cells(coord[0], coord[1], value = value)
+            self.update_cells(
+                coord[0], coord[1], 
+                value = value
+            )
 
     
     def can_move(self, shape, new_pos):
@@ -84,6 +92,8 @@ class Grid:
         """
         Moves the current shape down the grid if possible
         """
+        self.frame_counter += 1
+
         if not self.current_shape:
             return
 
@@ -91,24 +101,9 @@ class Grid:
             self.current_shape_location[0] + self.fall_speed_mult, # Adding 1 row
             self.current_shape_location[1]
         )
-        if self.can_move(self.current_shape, new_position):
-            self.draw_shape(
-                self.current_shape,
-                self.current_shape_location[0],
-                self.current_shape_location[1],
-                value = 0          # Erasing the previous position
-            )
-            self.current_shape_location = new_position
-            self.draw_shape(
-                self.current_shape, 
-                self.current_shape_location[0],
-                self.current_shape_location[1],
-            )
-        else:
-            new_position = (
-                self.current_shape_location[0] + 1, # Adding 1 row
-                self.current_shape_location[1]
-            )
+
+        if self.frame_counter % 8 == 0:    # number of frames per down move
+
             if self.can_move(self.current_shape, new_position):
                 self.draw_shape(
                     self.current_shape,
@@ -122,9 +117,37 @@ class Grid:
                     self.current_shape_location[0],
                     self.current_shape_location[1],
                 )
-
             else:
-                self.lock_shape()
+                new_position = (
+                    self.current_shape_location[0] + 1, # Adding 1 row
+                    self.current_shape_location[1]
+                )
+                if self.can_move(self.current_shape, new_position):
+                    self.draw_shape(
+                        self.current_shape,
+                        self.current_shape_location[0],
+                        self.current_shape_location[1],
+                        value = 0          # Erasing the previous position
+                    )
+                    self.current_shape_location = new_position
+                    self.draw_shape(
+                        self.current_shape, 
+                        self.current_shape_location[0],
+                        self.current_shape_location[1],
+                    )
+
+                else:
+                    self.lock_shape()
+
+
+        else:
+            
+            self.draw_shape(
+                self.current_shape,
+                self.current_shape_location[0],
+                self.current_shape_location[1]
+            )
+
 
 
     def move_side(self, side = 1):
@@ -151,6 +174,8 @@ class Grid:
                 self.current_shape_location[1],
             )
 
+
+
     def rotate(self):
         """
         Rotates the current shape
@@ -172,7 +197,6 @@ class Grid:
                 self.current_shape_location[1],
             )
 
-        
 
 
     def new_shape(self, shape):
@@ -191,10 +215,15 @@ class Grid:
         """
         Checks for completed rows after locking the current object
         """
+        num_completed = 0
         for i, row in enumerate(self.grid_list):
             if sum(row) == self.width*2:
+                num_completed += 1
                 del self.grid_list[i]
                 self.grid_list.insert(0, [0 for _ in range(self.width)])
+        if num_completed != 0:
+            self.score += self.points_granted[num_completed - 1] # Give points
+            self.speed *= .95 # Increase speed
 
 
     def lock_shape(self):
@@ -205,13 +234,12 @@ class Grid:
             self.current_shape,
             self.current_shape_location[0],
             self.current_shape_location[1],
-            value = 2
+            value = 2,
         )
         self.row_is_complete()
         self.current_shape = None
         new_shape = random.choice(SHAPES_LIST)
         self.new_shape(new_shape)
-        self.speed *= .95
 
 
     def print(self):
@@ -222,9 +250,7 @@ class Grid:
         print("+" + "-"*self.width + "+")
         for row in self.grid_list:
             print("|" + ''.join(["." if x == 0 else "#" for x in row]) + "|")
-        print("+" + "-"*self.width + "+", end='\r')
-
-
+        print("+" + "-"*self.width + f"+    Your score: {self.score}", end='\r')
 
 
 
@@ -232,23 +258,31 @@ def main():
     grid = Grid(G_WIDTH, G_HEIGHT, INIT_SPEED)
     grid.new_shape(random.choice(SHAPES_LIST))
 
+    last_action_time = 0
+    freeze_time = .1       # minimum time between actions
+
     while True:
 
         if grid.game_over:
             break
+
+        current_time = time.time()
+
         if keyboard.is_pressed('h'):
             grid.move_side(side = -1)
         if keyboard.is_pressed('l'):
             grid.move_side(side = 1)
         if keyboard.is_pressed('k'):
-            grid.rotate()
+            if current_time - last_action_time > freeze_time:
+                grid.rotate()
+                last_action_time = current_time
         if keyboard.is_pressed('j'):
             grid.fall_speed_mult = 2
         else:
             grid.fall_speed_mult = 1
 
-        grid.print()
         grid.move_down()
+        grid.print()
         time.sleep(grid.speed)
 
 
